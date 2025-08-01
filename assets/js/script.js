3c599e9a0d308049578b7bff37fd41e2819d6338
mishkanchik <mishkanchik594@gmail.com>
2025-08-01 23:10:28 +0300

    scroll element block

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,3 +133,21 @@ document.querySelectorAll('.service-card').forEach(card => {
         }
     });
 });
+
+
+
+
+document.querySelectorAll('#navmenu a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
